7f2d28c905ca5446e7e6a0d37132aa88a6e02eaa
KousalyaSundar <kousalya.sundar0701@gmail.com>
2025-07-21 21:25:48 +0530

    Add files via upload

---
+++ CheckNumberIsPositive.java
@@ -0,0 +1,18 @@
+package DatatypeAssignments;
+
+public class CheckNumberIsPositive {
+
+	public static void main(String[] args) {
+		int a=10;
+		if(a>0)
+		{
+			System.out.println(a + " " + "is positive");
+		}else if (a<0) {
+			System.out.println(a + " " + "is negative");
+		}
+		else {
+			System.out.println(a + " " + "is zero");
+		}
+	}
+
+}
