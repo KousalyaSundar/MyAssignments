90c14ed1995f66b2eea88531f6a580c12cd4eb48
KousalyaSundar <kousalya.sundar0701@gmail.com>
2025-07-24 10:55:34 +0530

    Add files via upload
    Assignment for data types

---
+++ Chrome.java
@@ -0,0 +1,20 @@
+package MyAssignments;
+public class Chrome {
+	public static void main(String[] args) {
+		//float
+		float version=91.0f;
+		System.out.println(version);
+		//String
+		String developer="Google";
+		System.out.println(developer);
+		//boolean
+		boolean isbeta=false;
+		System.out.println(isbeta);
+		//int
+		int releaseYear=2008;
+		System.out.println(releaseYear);
+		//char
+		char shortcutKey='C';
+		System.out.println(shortcutKey);
+	}
+}
